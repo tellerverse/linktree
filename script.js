55ce919bef2862abcd9a02f9b23a1960d17c0db1
tellerverse <156932909+tellerverse@users.noreply.github.com>
2025-10-26 14:23:22 +0200

    Add event listener to change text on button click

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeText').addEventListener('click', () => {
+  const text = document.getElementById('text');
+  text.textContent = 'Du hast den Text per JavaScript geändert!';
+});
